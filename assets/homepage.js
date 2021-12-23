6890fef10dfa57e270f8b6652e89b07ebd844b29
Claire DeLargy <clairedelargy@gmail.com>
2021-12-23 10:14:45 -0600

    added notes to all files

--- assets/homepage.js
+++ assets/homepage.js
@@ -1,3 +1,6 @@
+// JS file to add functionality to our webpage
+
+// List of global variables
 var pokeForm = document.querySelector('#user-form');
 var nameInput = document.querySelector('#pokeName');
 var pokeSearchName = document.querySelector('#poke-search');
@@ -6,6 +9,7 @@ var priceList = document.querySelector('#poke-prices');
 var spriteBox = document.querySelector('#searchedSprite');
 var loading=document.querySelector('#loading');
 
+// Function to catch user input
 var submitName = function (event) {
   // prevent page from refreshing
   event.preventDefault();
@@ -16,11 +20,13 @@ var submitName = function (event) {
   if (pokemonName) {
     loadPokemon (pokemonName)
 
+     // Calls modal used if user did not enter in any information
   } else {
     $('#myModal').modal('toggle');
   }
 };
 
+// First API call - gathers name, Pokemon card Image, and value of card selected
 var getPokeCardAndValue = function (pokemonName) {
   //shows pokedex once function is called
   loading.classList.remove("hidden")
@@ -35,16 +41,19 @@ var getPokeCardAndValue = function (pokemonName) {
       if (response.ok) {
         console.log(response);
         response.json().then(function (data) {
+          // Chooses first pokemone in array of search pokemon name, narrows down the specific information we want to use (image, price)
           console.log(data.data[0].images);
           console.log(data.data[0].cardmarket.prices);
+          // Assigns these items to variables
           let pokeCard = data.data[0].images;
           let pokePrice = data.data[0].cardmarket.prices;
           let pokeName = data.data[0].name;
+          // Displays these variables on the webpage
           imgBox.src = pokeCard.small
           pokeSearchName.textContent = pokeName
           priceList.textContent = "$" + pokePrice.averageSellPrice
          
-
+        // Store user input data to local storage
         var storedHistory = window.localStorage.getItem("history")
         var history = []
         if (storedHistory !== null) {
@@ -60,17 +69,18 @@ var getPokeCardAndValue = function (pokemonName) {
         nameInputEl.value = ''
 
         });
-
+        // Calls modal used if user did not enter in any information
       } else {
         $('#myModal').modal('toggle');
       }
     })
+    // Calls modal is server is not loading correctly
     .catch(function (error) {
       $('#brokenModal').modal('toggle');
     });
 };
 
-
+// Load user data from local storage and add a button for user to access
 var loadHistory = function () {
   var historyEl = document.getElementById("history")
   historyEl.innerHTML = ""
@@ -91,8 +101,8 @@ var loadHistory = function () {
     historyEl.appendChild(historyButtonEl)
   };
 };
-// This is the API call from the other website - not sure how to get both show up
 
+// Second API call - shows pokemon sprite image for a more pleasing view for user
 var getPokeSprite = function(pokemonName) {
   // allows name to be in lowercase or uppercase
   var apiUrl = 'https://pokeapi.co/api/v2/pokemon/' + pokemonName.toLowerCase();
@@ -103,14 +113,19 @@ var getPokeSprite = function(pokemonName) {
       if (response.ok) {
         console.log(response);
         response.json().then(function(data) {
+          // Chooses just the sprite image data from server
           console.log(data.sprites.front_default);
+          // Assigns this data to a variable
           let pokeSprite = data.sprites.front_default
+          // Displays variable on webpage
           spriteBox.src = pokeSprite
         });
+        // Calls modal used if user did not enter in any information
       } else {
         alert('Error: Pokemon was not found, please try again!' + response.statusText);
       }
     })
+    // Calls modal is server is not loading correctly
     .catch(function(error) {
       alert('Unable to connect');
     });
@@ -120,6 +135,7 @@ var loadPokemon = function (pokemonName) {
   getPokeCardAndValue(pokemonName);
   getPokeSprite(pokemonName)
 }
-
+// Adds functionality so the function runs when the submit button is clicked
 pokeForm.addEventListener('submit', submitName);
+// Function to pull data from local storage
 loadHistory();
